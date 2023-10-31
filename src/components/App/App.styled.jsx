4b638b3b39a1a869a1f4a04134b104ca61ed588d
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2023-10-31 19:35:56 +0300

    Initial commit

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 400px;
+  padding: 10px;
+margin-left: 100px;
+margin-right: auto;
+
+`;
+
+export const Wrapper = styled.div`
+  text-align: center;
+  font-size: 20px;
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+  
+  font-style: oblique;
+`;
+
+export const SubTitle = styled.h2`
+  text-align: center;
+  
+  font-style: italic;
+`;
